56f7dfdad3dfa8780a8e3943bf592323f7168628
peri <peri@1password.com>
2021-04-08 17:51:53 -0700

    na

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 var request = require("request");
 
 var data = {
-    'url': 'https://audd.tech/example1.mp3',
-    'return': 'apple_music,spotify',
-    'api_token': 'test'
+    'api_token': '68396951c3d5446143ce791b388bb5dd',
+    'url': 'https://www.youtube.com/watch?v=p84O3JAp_IM',
+    'skip': '3',
+    'every': '1',
+    'accurate_offsets': '1',
 };
 
 request({
-    uri: 'https://api.audd.io/',
+    uri: 'https://enterprise.audd.io/',
     form: data,
     method: 'POST'
   }, function (err, res, body) {
