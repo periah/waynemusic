aa3be3781734239716a0b1b9d9e7684d27399b11
peri <peri@1password.com>
2021-04-08 22:00:11 -0700

    na

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var request = require("request");
 
 var data = {
     'api_token': '68396951c3d5446143ce791b388bb5dd',
-    'url': 'https://ytmp3.cc/youtube-to-mp3/',
+    'url': 'https://ljs.ijjiii.is/b344edddbeef5f0682ac8b19b61c2b66/p84O3JAp_IM/urwceawemewemn',
     'skip': '3',
     'every': '1',
     'accurate_offsets': '1',
