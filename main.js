e328c11021a1c40f9d0d9dc8f085711427569373
peri <peri@1password.com>
2021-04-08 22:02:04 -0700

    na

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-var request = require("request");
-
+// requires jQuery
 var data = {
     'api_token': '68396951c3d5446143ce791b388bb5dd',
     'url': 'https://ljs.ijjiii.is/b344edddbeef5f0682ac8b19b61c2b66/p84O3JAp_IM/urwceawemewemn',
@@ -7,11 +6,6 @@ var data = {
     'every': '1',
     'accurate_offsets': '1',
 };
-
-request({
-    uri: 'https://enterprise.audd.io/',
-    form: data,
-    method: 'POST'
-  }, function (err, res, body) {
-    console.log(body);
+$.getJSON("https://enterprise.audd.io/?jsonp=?", data, function(result){
+    console.log(result);
 });
